16c9fe41fb029f2c1a0f593d31209ac1bbd77267
shsm385 <shsm385@gmail.com>
2017-01-10 14:44:56 +0900

    TA updata

--- TA/check.rb
+++ TA/check.rb
@@ -11,11 +11,11 @@ def read_file
 	  line.slice!('5')
 	  a.push(line)
 	end
-  }  
+  }
   return a
 end
 
-def read_test  
+def read_test
   a = []
   File.open(ARGV[1]){|f|
     f.each_line do |line|
@@ -26,7 +26,7 @@ def read_test
 	  line.slice!('5')
 	  a.push(line)
 	end
-  }  
+  }
   return a
 end
 
@@ -35,9 +35,25 @@ def flat(a,b)
 end
 
 def check(a)
-  return a.select{|e| a.index(e)!=a.rindex(e)}.uniq 
+  return a.select{|e| a.index(e)!=a.rindex(e)}.uniq
 end
 
+def check2(a)
+  for var in a do
+    count = 0
+    temp = var
+    for var2 in a do
+      if var2.include?(temp)
+        count = count + 1
+        if count > 1
+          puts temp
+        end
+      end
+    end
+  end
+end
 
+puts "テストとデータのチェック"
 puts check(flat(read_file,read_test))
- 
+puts "データとデータのチェック"
+check2(read_file)
